59c8b9dc2cf399e963423ceeba5c7d99c64f0894
Venkateshwaran-0a7i <venkateshwaran0720@gmail.com>
2026-08-31 00:22:53 +0530

    fix: app crash (AppCompat theme + Room v3) and icon regenerated from Wallet-Scholer.png

--- app/src/main/java/com/walletscholer/app/data/local/AppDatabase.kt
+++ app/src/main/java/com/walletscholer/app/data/local/AppDatabase.kt
@@ -16,7 +16,7 @@ import com.walletscholer.app.data.model.UserSettingsEntity
         GoalEntity::class,
         UserSettingsEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
